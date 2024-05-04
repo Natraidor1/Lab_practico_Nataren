333016ff42b1c2dda8b941523da194fad5eae3da
Nataren <20220594@ricaldone.edu.sv>
2024-05-03 18:37:25 -0600

    primer commit

---
+++ app/src/main/java/francisco/nataren/emulador_20220594_francisco2/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package francisco.nataren.emulador_20220594_francisco2.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/francisco/nataren/emulador_20220594_francisco2/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package francisco.nataren.emulador_20220594_francisco2.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
